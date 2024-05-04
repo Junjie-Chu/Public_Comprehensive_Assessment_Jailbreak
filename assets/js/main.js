30f380d1142a90fad347d75b9bf53a80cfcf1ed9
Junjie Chu <65893273+Junjie-Chu@users.noreply.github.com>
2024-05-04 18:15:49 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ $(function () {
 $(document).ready(function() {
     $('#example').DataTable({
         // 在这里加入DataTable的配置选项
-        "paging":   true, // 开启表格的分页功能
+        "paging":   false, // 开启表格的分页功能
         "ordering": true, // 启用表格的排序功能
         "info":     true,  // 显示底部信息
         "scrollX": true, // 启用水平滚动
