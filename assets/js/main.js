3c0d9eb2a38feac21669aab14e3bb091d81feaac
Junjie Chu <65893273+Junjie-Chu@users.noreply.github.com>
2024-05-04 13:53:03 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,13 @@ $(function () {
   initSearchBox();
   styleContentToMD();
 });
+
+$(document).ready(function() {
+    $('#example').DataTable({
+        // 在这里加入DataTable的配置选项
+        "paging":   true, // 开启表格的分页功能
+        "ordering": true, // 启用表格的排序功能
+        "info":     true,  // 显示底部信息
+        "scrollX": true, // 启用水平滚动
+    });
+});
